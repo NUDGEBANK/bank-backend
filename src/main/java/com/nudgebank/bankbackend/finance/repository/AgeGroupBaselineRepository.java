12d3a364e004e50ad49ebd2e009ef7b900184038
GAEUN JEONG <rkdms357@naver.com>
2026-04-06 10:36:55 +0900

    Feat: 연령대별 소비 기준 테이블 및 레포지토리 추가

---
+++ src/main/java/com/nudgebank/bankbackend/finance/repository/AgeGroupBaselineRepository.java
@@ -0,0 +1,10 @@
+package com.nudgebank.bankbackend.finance.repository;
+
+import com.nudgebank.bankbackend.finance.domain.AgeGroupBaseline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AgeGroupBaselineRepository extends JpaRepository<AgeGroupBaseline, Long> {
+    Optional<AgeGroupBaseline> findByAgeGroup(String ageGroup);
+}
