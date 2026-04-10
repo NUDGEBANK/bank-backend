d8a64db9894f5e495b827b6c5b3a98f27958da73
SuYun25 <hamsuyun67@gmail.com>
2026-04-10 14:26:56 +0900

    Feat: 자기계발 대출 자격증 우대금리 반영

--- src/main/java/com/nudgebank/bankbackend/loan/repository/LoanRepository.java
+++ src/main/java/com/nudgebank/bankbackend/loan/repository/LoanRepository.java
@@ -8,6 +8,8 @@ public interface LoanRepository extends JpaRepository<Loan, Long> {
 
     Optional<Loan> findTopByMember_MemberIdOrderByStartDateDescIdDesc(Long memberId);
 
+    Optional<Loan> findTopByLoanApplication_IdOrderByIdDesc(Long loanApplicationId);
+
     Optional<Loan> findTopByMember_MemberIdAndLoanApplication_LoanProduct_LoanProductTypeOrderByStartDateDescIdDesc(
         Long memberId,
         String loanProductType
